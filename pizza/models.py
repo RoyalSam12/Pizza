d29451ac5f72e586f6f3058b4010b4a53fd20573
RoyalSam12 <67439524+RoyalSam12@users.noreply.github.com>
2020-09-30 15:07:12 +0300

    version 1

--- pizza/models.py
+++ pizza/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class Pizza(models.Model):
     pizza_name = models.CharField(max_length=30)
-    category = models.ForeignKey(Category, on_delete=None)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
