ba57bd6d2caf806b7db758fb897be12438045021
RoyalSam12 <67439524+RoyalSam12@users.noreply.github.com>
2020-09-30 15:00:24 +0300

    version 1

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Pizza(models.Model):
+    pizza_name = models.CharField(max_length=30)
+    category = models.ManyToOneRel(Category,)
